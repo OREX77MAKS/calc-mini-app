75b6948197c59f916ecda22da4f92cf3918df8b7
OREX77MAKS <telik0001telik@gmail.com>
2025-07-13 22:00:12 +0200

    Fix code in files

---
+++ script.js
@@ -0,0 +1,21 @@
+const tg = window.Telegram.WebApp;
+tg.ready();
+tg.expand();
+
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Ошибка';
+    }
+}
